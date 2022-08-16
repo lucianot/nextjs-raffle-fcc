2044fd32bab02dc42901fa86daca307e24e94137
Luciano <luciano@Lucianos-MacBook-Pro.local>
2022-08-16 18:39:47 -0300

    added tailwind and basic styles

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -6,18 +6,21 @@ import { ethers } from "ethers"
 import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
-    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled, Moralis } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
     const [entranceFee, setEntranceFee] = useState("0")
     const [numPlayers, setNumPlayers] = useState("0")
     const [recentWinner, setRecentWinner] = useState("0")
     const [provider, setProvider] = useState()
-    const { Moralis } = useMoralis()
 
     const dispatch = useNotification()
 
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -91,23 +94,29 @@ export default function LotteryEntrance() {
     }
 
     return (
-        <div>
+        <div className="p-5">
             Hi from lottery entrance!
             {raffleAddress ? (
                 <div>
                     <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         onClick={async function () {
                             await enterRaffle({
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
                         }}
+                        disabled={isLoading || isFetching}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
                     </button>
-                    Entrance fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH Number of
-                    players: {numPlayers}
-                    Recent winner: {recentWinner}
+                    <div>Entrance fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>Number of players: {numPlayers}</div>
+                    <div>Recent winner: {recentWinner}</div>
                 </div>
             ) : (
                 <div>No raffle address detected!</div>
